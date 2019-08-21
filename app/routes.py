bbec61341821674d6808136085ee891550bfab65
erfaenda <regalko7@gmail.com>
2019-08-21 16:18:50 +0500

    puk

--- app/routes.py
+++ app/routes.py
@@ -7,5 +7,24 @@ from flask import render_template
 
 def index():
     user = {'username': 'Alex'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'Alex'},
+            'body': 'Круто я учу фласк!'
+        },
+        {
+            'author': {'username': 'Миша'},
+            'body': 'Какой прекрасный денек!'
+        },
+        {
+            'author': {'username': 'Валера'},
+            'body': 'Опять хуйню какую то программируешь?'
+        },
+        {
+            'author': {'username': 'Тень прогера'},
+            'body': 'Круто! Стоит на основе этого простого примера сделать что то типо примитивного хтмл чата!'
+        }
+
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
     #return "А если по русский написать?"
