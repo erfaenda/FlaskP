04bba0296ce7be28325a22bacdeca3bea92b9aa0
erfaenda <regalko7@gmail.com>
2019-08-21 15:54:20 +0500

    puk

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return "А если по русский написать?"
+    user = {'username': 'Alex'}
+    return render_template('index.html', title='Home', user=user)
+    #return "А если по русский написать?"
